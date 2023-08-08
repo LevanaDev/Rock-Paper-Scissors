83605dcf2e9be2bd3fd1e1b27fadb42aeda583a6
LevanaDev <139725301+LevanaDev@users.noreply.github.com>
2023-08-07 20:12:27 -0600

    Modify index.html

--- Rock-paper-scissors.js
+++ Rock-paper-scissors.js
@@ -1,23 +1,50 @@
+const select = document.querySelector("select");
+const para = document.querySelector("p");
+const btn = document.querySelector("button");
+select.addEventListener("change", playerSelectionQuery);
+btn.addEventListener("click",playRound);
+
 //Function for the computer to have a random choice 
 function getComputerChoice(){
     // Array with the possible choices
-    const choices = [ "Rock", "Paper", "Scissor" ];
+    const choices = [ "Rock", "Paper", "Scissors" ];
     //Uses Math.floor and Math.random for get the random choice
     ComputerChoice = choices[ Math.floor( Math.random()  * choices.length ) ];
     return ComputerChoice;
 }
 // Function to ask the user for a choice
 function playerSelection(){
-    let playerChoice = prompt("Write Rock, Paper or Scissor");
+    let playerChoice = prompt("Write Rock, Paper or Scissors");
     playerChoice = playerChoice.toLowerCase();
-    if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissor"){
-        return "Invalid choice"
+    if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors"){
+        return "Invalid choice";
     }  
     playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.substring(1);
     return playerChoice;
 }
+
+function playerSelectionQuery(){
+    const choice = select.value;
+    if(choice === "Rock"){
+        para.textContent = "You select Rock.";
+        return choice;
+    }
+    else if(choice === "Paper"){
+        para.textContent = "You select Rock.";
+        return choice;
+    }
+    else if(choice === "Scissors"){
+        para.textContent = "You select Rock.";
+        return choice;
+    }
+    else{
+        para.textContent = "";
+    }
+}
+
 //Function to plays a single round of Rock Paper Scissors
 function playRound(){
+    //let playerChoice = playerSelectionQuery();
     let playerChoice = playerSelection();
     let computerChoice = getComputerChoice();
     let messageWin = `You Win! ${playerChoice} beats ${computerChoice}`;
@@ -27,19 +54,19 @@ function playRound(){
         if(computerChoice === "Rock" && playerChoice === "Paper"){
             return messageWin;
         }
-        if(computerChoice === "Rock" && playerChoice === "Scissor"){
+        if(computerChoice === "Rock" && playerChoice === "Scissors"){
             return messageLose;
         } 
-        if(computerChoice === "Paper" && playerChoice === "Scissor"){
+        if(computerChoice === "Paper" && playerChoice === "Scissors"){
             return messageWin;
         }
         if(computerChoice === "Paper" && playerChoice === "Rock"){
             return messageLose;
         }
-        if(computerChoice === "Scissor" && playerChoice === "Rock"){
+        if(computerChoice === "Scissors" && playerChoice === "Rock"){
             return messageWin;
         }
-        if(computerChoice === "Scissor" && playerChoice === "Paper"){
+        if(computerChoice === "Scissors" && playerChoice === "Paper"){
             return messageLose;
         }  
     } 
